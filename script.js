2f067d19cc4a28a238d742d7f42023d28a35731c
Z3key <galaxy.s5.n950a@gmail.com>
2025-10-25 16:01:32 +0300

    AutomClock (local time)

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-function updateClock(time) {
+// function updateClock(time) {
+function updateClock() {
     const now = new Date();
-    const hours = String(now.getUTCHours() + time);
-    const minutes = String(now.getUTCMinutes());
-    const seconds = String(now.getUTCSeconds());
+    // const hours = String(now.getUTCHours() + time);
+    const hours = String(now.getUTCHours() + 3).padStart(2, '0');
+    const minutes = String(now.getUTCMinutes()).padStart(2, '0');
+    const seconds = String(now.getUTCSeconds()).padStart(2, '0');
 
     document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
 
     // console.log(`${hours}:${minutes}:${seconds}`);
 
-    const selectTimeZone = document.getElementById('timeZone');
-    timeZone = parseInt(selectTimeZone.value, 10);
-    selectTimeZone.textContent = timeZone;
+    // const selectTimeZone = document.getElementById('timeZone');
+    // timeZone = parseInt(selectTimeZone.value, 10);
+    // // selectTimeZone.textContent = timeZone;
+    // selectTimeZone.addEventListener('change', updateClock(timeZone))
 
 }
 
@@ -19,14 +22,16 @@ function updateClock(time) {
 // console.log(new Date().getUTCMinutes());
 // console.log(new Date().getUTCSeconds());
 
-let timeZone;
+// let timeZone;
 
-timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+// timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-let diffrent_time_UTC = new Date().getTimezoneOffset() / 60;
+// let diffrent_time_UTC = new Date().getTimezoneOffset() / 60;
 // console.log(timeZone)
 // console.log(diffrent_time_UTC)
 
 // updateClock(timeZone);
 // updateClock(9);
-setInterval(updateClock(9), 1000);
+// setInterval(updateClock(9), 1000);
+// setInterval(updateClock(timeZone), 1000);
+setInterval(updateClock, 1000);
