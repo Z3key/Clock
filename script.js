227b3e3300c277a4f31319d805a18a07b81151e4
Z3key <galaxy.s5.n950a@gmail.com>
2025-10-25 16:22:25 +0300

    Clear code

--- script.js
+++ script.js
@@ -1,37 +1,11 @@
-// function updateClock(time) {
 function updateClock() {
     const now = new Date();
-    // const hours = String(now.getUTCHours() + time);
     const hours = String(now.getUTCHours() + 3).padStart(2, '0');
     const minutes = String(now.getUTCMinutes()).padStart(2, '0');
     const seconds = String(now.getUTCSeconds()).padStart(2, '0');
 
     document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
 
-    // console.log(`${hours}:${minutes}:${seconds}`);
-
-    // const selectTimeZone = document.getElementById('timeZone');
-    // timeZone = parseInt(selectTimeZone.value, 10);
-    // // selectTimeZone.textContent = timeZone;
-    // selectTimeZone.addEventListener('change', updateClock(timeZone))
-
 }
 
-// console.log(new Date());
-// console.log(new Date().getUTCHours() + 9);
-// console.log(new Date().getUTCMinutes());
-// console.log(new Date().getUTCSeconds());
-
-// let timeZone;
-
-// timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-
-// let diffrent_time_UTC = new Date().getTimezoneOffset() / 60;
-// console.log(timeZone)
-// console.log(diffrent_time_UTC)
-
-// updateClock(timeZone);
-// updateClock(9);
-// setInterval(updateClock(9), 1000);
-// setInterval(updateClock(timeZone), 1000);
 setInterval(updateClock, 1000);
